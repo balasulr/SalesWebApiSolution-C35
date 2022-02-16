84e296457c9ccc2fe42d9defdd0264d27b908e1a
Lakshmi Balasubramaniam <lrb697@gmail.com>
2022-02-16 16:26:50 -0500

    Add project files.

---
+++ SalesWebApi/Models/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SalesWebApi.Models {
+    
+    public class AppDbContext : DbContext {
+        // Defining Customer Model
+        public virtual DbSet<Customer> Customers { get; set; } // Customers is the plural version
+        // Constructor with one parameter since don't need the default constructor
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options) { }
+        // FLuentAPI - Makes non primary key column unique
+        protected override void OnModelCreating(ModelBuilder builder) {
+
+        }
+    }
+}
