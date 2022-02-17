66e3bf77cf9d70dca3bcc50b94dba1f09cff8b8a
Lakshmi Balasubramaniam <lrb697@gmail.com>
2022-02-17 10:17:17 -0500

    Created Order Class in Models Folder with properties & attributes. Added migration & updated database. Added Controller for Order Model

--- SalesWebApi/Migrations/20220217142719_Added Order Class.Designer.cs
+++ SalesWebApi/Migrations/20220217142719_Added Order Class.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SalesWebApi.Models;
 
 namespace SalesWebApi.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220217142719_Added Order Class")]
+    partial class AddedOrderClass
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- SalesWebApi/Models/AppDbContext.cs
+++ SalesWebApi/Models/AppDbContext.cs
@@ -5,8 +5,11 @@ namespace SalesWebApi.Models {
     public class AppDbContext : DbContext {
         // Defining Customer Model
         public virtual DbSet<Customer> Customers { get; set; } // Customers is the plural version
+        public virtual DbSet<Order> Orders { get; set; }
+        
         // Constructor with one parameter since don't need the default constructor
         public AppDbContext(DbContextOptions<AppDbContext> options): base(options) { }
+
         // FLuentAPI - Makes non primary key column unique
         protected override void OnModelCreating(ModelBuilder builder) {
 
