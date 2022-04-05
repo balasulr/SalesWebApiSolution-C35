5113f8b9437c53841b4c943d7c62a38365cc3b5a
Lakshmi Balasubramaniam <lrb697@gmail.com>
2022-04-05 11:06:29 -0400

    Added Employee Model, Controller & Migration

--- SalesWebApi/Models/AppDbContext.cs
+++ SalesWebApi/Models/AppDbContext.cs
@@ -7,6 +7,7 @@ namespace SalesWebApi.Models {
         public virtual DbSet<Customer> Customers { get; set; }
         public virtual DbSet<Order> Orders { get; set; }
         public virtual DbSet<Orderline> Orderlines { get; set; }
+        public virtual DbSet<Employee> Employee { get; set; }
         
         // Constructor with one parameter since don't need the default constructor
         public AppDbContext(DbContextOptions<AppDbContext> options): base(options) { }
---
+++ SalesWebApi/Models/Employee.cs
@@ -0,0 +1,8 @@
+﻿namespace SalesWebApi.Models {
+    public class Employee{
+
+        public int Id { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+    }
+}
